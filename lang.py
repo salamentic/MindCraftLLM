d10d2ce15b12cdef1521266605774cf8d793b67c
salamentic <raniruddh99@gmail.com>
2023-04-21 14:58:13 -0400

    Added workflow for all games with a pickle file. TODO: GPT Prompts

--- lang.py
+++ lang.py
@@ -1,36 +1,43 @@
 import os
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Annoy
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+
 import pickle
+from glob import glob
 
-os.environ["OPENAI_API_KEY"] = "..."
+hf = HuggingFaceEmbeddings()
+k = 2
 
-game_captions = []
-with open("Image2Paragraph/image2textMindcraft/141_212_108_99_20210325_121618.pkl", "rb") as game_file:
-    game_captions = pickle.load(game_file)
-print(len(game_captions))
-'''
-TODO: Check if this is needed, maybe not given all our data is in lists?
+def chagpt_questioning(agent, top_k_q1, question1):
+    return
 
-loader = TextLoader("test.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+for game_path in glob("Image2Paragraph/image2textMindcraft/*.pkl"):
+    game_captions = [] # Store captions for each frame
+    game_agent = None # Agent that will reason over current game
+    with open(game_path, "rb") as game_file:
+        game_captions = pickle.load(game_file)
+    print(game_path, len(game_captions))
 
-docs[:5]
-'''
+    game_index = Chroma.from_texts(["Nothing"],hf)
+    game_recency = []
+    questions = [None]*len(game_captions) # Array to store questions, no question = None
 
+    for i_s, scene in enumerate(game_captions):
+        # Game_index for q1,q2; game_recency for time based retrieval for q3 
+        game_index.add_texts([scene])
+        game_recency.append(scene)
 
-model_name = "sentence-transformers/all-mpnet-base-v2"
-model_kwargs = {'device': 'cuda'}
+        # If this timestep has an associated question, ask chatgpt the questions
+        if questions[i_s]:
+            top_k_q1 = game_index.similarity_search(question1)[:k]
+            generated_answer_q1 = chatgpt_questioning(agent, top_k_q1, question1)
 
-openai = OpenAIEmbeddings(openai_api_key="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")
-frame_index = Annoy.from_texts(game_captions, openai)
-frame_index.save_local("game_stores/141_212_108_99_20210325_121618")
-print(frame_index)
-top_k_frames = frame_index.similarity_search("Has your partner made Blue Wool?", k=3)
+            top_k_q2 = game_index.similarity_search(question2)[:k]
+            generated_answer_q2 = chatgpt_questioning(agent, top_k_q2, question2)
 
-print(top_k_frames)
-print("finish")
+            generated_answer_q3 = chatgpt_questioning(agent, game_recency[:k], question3)
+
+            print(generated_answer_q1, generated_answer_q2, generated_answer_q3)
+            # TODO: Get answer from result
+
+    print(game_path, "finish")
